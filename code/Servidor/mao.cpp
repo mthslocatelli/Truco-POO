276ed94fcca4b0020225e9b30f06c6e00d3815b5
Matheus <matheusdelima2001@gmail.com>
2025-07-13 16:43:23 -0300

    Refazendo cagada

--- code/Servidor/mao.cpp
+++
@@ -1,9 +0,0 @@
-
-#include "mao.h"
-
-void Hand::deal(Baralho& deck, Jogador& human, JogadorBot& bot) {
-    for (int i = 0; i < 3; ++i) {
-        human.receberCarta(deck.distribuir());
-        bot.receberCarta(deck.distribuir());
-    }
-}
